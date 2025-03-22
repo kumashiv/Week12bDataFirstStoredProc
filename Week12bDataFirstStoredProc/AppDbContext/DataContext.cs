671c144395af8c8ab9e021d519759df8fbce4698
shiv kumar <kumarshiv_sk@outlook.com>
2025-03-22 00:53:47 -0400

    1. Added Connection database string 2. Installed Nuget packages 3. Added AppDbContext folder with class 4. Models Employee.cs created

---
+++ Week12bDataFirstStoredProc/AppDbContext/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Week12bDataFirstStoredProc.Models;
+
+namespace Week12bDataFirstStoredProc.AppDbContext
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Employee> Employees { get; set; }  // Table Name
+    }
+}
